10007691a34b7bc11518e74e3119dc2c3eb9537f
ntmk202 <khue131@gmail.com>
2024-05-04 20:32:58 +0700

    origin

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('urlPage', views.urlPage, name='url_Page'),
+    path('document', views.document, name='document'),
+    path('audio', views.audio, name='audio'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html', {})
+
+def urlPage(request):
+    return render(request, 'urlPage.html', {})
+
+def document(request):
+    return render(request, 'document.html', {})
+
+def audio(request):
+    return render(request, 'audio.html', {})
+
