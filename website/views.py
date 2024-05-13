774bf8d03616c27c24fd1dd973f7fb1f355c9a3f
ntmk202 <khue131@gmail.com>
2024-05-14 00:44:31 +0700

    add normal summarization

--- website/views.py
+++ website/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from transformers import pipeline, AutoTokenizer
+
 
 def home(request):
     fname = request.user.first_name if request.user.is_authenticated else None
-    return render(request, 'index.html', {'fname': fname})
+    if request.method == 'POST':
+        input_text = request.POST.get('input_text')
+        tokenizer_path = "D:/python/NLP/django_text-summarize/text_summarizer/website/model/tokenizer-summarization"
+        model_path = "Falconsai/text_summarization"
+        # tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+        summarizer = pipeline("summarization", model=model_path, framework='tf')
+        summary = summarizer(input_text, max_length=1000, min_length=30, do_sample=False)
+        summarized_text = summary[0]['summary_text']
+        return render(request, 'index.html', {'summarized_text': summarized_text, 'input_text': input_text, 'fname': fname})
+    else:
+        return render(request, 'index.html', {'fname': fname})
 
 def urlPage(request):
     fname = request.user.first_name if request.user.is_authenticated else None
@@ -63,3 +75,17 @@ def signout(request):
     logout(request)
     messages.success(request, "Logged out successfully!")
     return redirect("home")
+  
+
+def test(request):
+    if request.method == 'POST':
+        input_text = request.POST.get('input_text')
+        tokenizer_path = "D:/python/NLP/django_text-summarize/text_summarizer/website/model/tokenizer-summarization"
+        model_path = "D:/python/NLP/django_text-summarize/text_summarizer/website/model/pegasus-samsum-model"
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+        summarizer = pipeline("summarization", model=model_path,tokenizer=tokenizer, framework='tf')
+        summary = summarizer(input_text, length_penalty= 0.8, num_beams=8, max_length= 1028)
+        summarized_text = summary[0]['summary_text']
+        return render(request, 'test.html', {'summarized_text': summarized_text})
+    else:
+        return render(request, 'test.html', {})
